da31dd064262b7847066315bc81b527b05943699
Hector Gonzalez <gorozco.hector@gmail.com>
2022-11-15 17:13:34 -0600

    Make mock messages

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mini Message Board
+By Héctor González Orozco
+
+Message board that can perform basic CRUD operations.
+First app made with Express.
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
+const date = require('date-fns');
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: date.formatDistanceToNow(new Date()),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: date.formatDistanceToNow(new Date()),
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {title: "Mini Messageboard", messages });
 });
 
 module.exports = router;
